07f91c62f2e96d66e2ddbb93db707e3bf18acba7
mahmoudakrm20 <mahmoudakrm20@gmail.com>
2025-07-01 14:07:27 +0300

    Patient page and model

---
+++ CodeZoneTraining/Models/PatientModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace CodeZoneTraining.Models
+{
+    public class PatientModel
+    {
+        public string? NameArabic { get; set; }
+        public string? NameEnglish { get; set; }
+        public string? Email { get; set; }
+    }
+
+
+}
