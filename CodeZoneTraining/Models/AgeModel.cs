ce9d27c8a6fe9230bdea5410daecee04aaf088e7
Mahmoud Akram <mahmoudakrm20@gmail.com>
2025-07-02 11:15:19 +0300

    AgeModel

---
+++ CodeZoneTraining/Models/AgeModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CodeZoneTraining.Models
+{
+    public class AgeModel
+    {
+        public int Years { get; set; }
+        public int Months { get; set; }
+        public int Days { get; set; }
+    }
+}
