57868c4f741812bea30d00a2c8886d4cd4efcb25
mahmoudakrm20 <mahmoudakrm20@gmail.com>
2025-07-06 14:12:23 +0300

    CZ File Upload Component

--- CodeZoneTraining/Models/PatientModel.cs
+++ CodeZoneTraining/Models/PatientModel.cs
@@ -11,6 +11,7 @@ namespace CodeZoneTraining.Models
         public string? NationalId { get; set; }
         public DateOnly? DateOfBirth { get; set; }
         public IBrowserFile? ProfileImage { get; set; }
+        public IBrowserFile? Attachment { get; set; }
 
     }
 }
